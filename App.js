d9419431e3270ef681600fb2ff0c26ad4713d2d9
vatsal <vatsal.palan@somaiya.edu>
2020-06-27 12:38:54 +0530

    final

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import Main from "./App/components/Main";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App/components/Main.js
@@ -0,0 +1,133 @@
+// import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+import Note from "./Note";
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: "",
+    };
+  }
+
+  render() {
+    let notes = this.state.noteArray.map((val, key) => {
+      return (
+        <Note
+          key={key}
+          keyval={key}
+          val={val}
+          deleteMethod={() => this.deleteNote(key)}
+        />
+      );
+    });
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headertext}>--TODO--</Text>
+        </View>
+
+        <ScrollView style={styles.scrollcontainer}>{notes}</ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(noteText) => this.setState({ noteText })}
+            value={this.state.noteText}
+            placeholder=">>Enter"
+            placeholderTextColor="white"
+            underlineColorAndroid="transparent"
+          ></TextInput>
+        </View>
+
+        <TouchableOpacity
+          onPress={this.addNote.bind(this)}
+          style={styles.addButton}
+        >
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  addNote() {
+    if (this.state.noteText) {
+      var d = new Date();
+      this.state.noteArray.push({
+        date: d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate(),
+        note: this.state.noteText,
+      });
+      this.setState({ noteArray: this.state.noteArray });
+      this.setState({ noteText: "" });
+    }
+  }
+
+  deleteNote(key) {
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "#E91E63",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd",
+  },
+  headertext: {
+    color: "white",
+    fontSize: 20,
+    padding: 26,
+  },
+  scrollcontainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+  },
+  textInput: {
+    alignSelf: "stretch",
+    color: "#fff",
+    padding: 20,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderTopColor: "#ededed",
+  },
+  addButton: {
+    position: "absolute",
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "#E91E63",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8,
+  },
+  addButtonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
+});
